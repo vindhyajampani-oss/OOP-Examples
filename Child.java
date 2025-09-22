225c90cb1f122d22bd7b092871128d20428895fe
vindhya_28 <vindhyajampani@gmail.com>
2025-09-22 16:14:25 +0530

    Add files via upload

---
+++ Child.java
@@ -0,0 +1,9 @@
+package com.codegnan.OOPexamples;
+
+//sub-class extends super-class
+//new class extends old class
+public class Child extends Parent {
+public void methodTwo() {
+	System.out.println("Method two executed");
+}
+}
---
+++ Parent.java
@@ -0,0 +1,9 @@
+package com.codegnan.OOPexamples;
+
+//super class
+public class Parent {
+	public void methodOne() {
+		System.out.println("Method one executed");
+	}
+
+}
